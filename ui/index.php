adc7fe5638dd3412b7140a31fa39af6c9656f3a6
Klemen Košir <klemen913@gmail.com>
2012-05-23 19:04:17 +0200

    Corrected grammar
    Recommit due to data loss.

--- ui/index.php
+++ ui/index.php
@@ -13,19 +13,18 @@
 ##                                                                             ##
 #################################################################################
 
-
 define( "LANGUI_INDX_T1", "Username" );
 define( "LANGUI_INDX_T2", "Password" );
 define( "LANGUI_INDX_T3", "Login" );
-define( "LANGUI_INDX_T4", "Players" );
-define( "LANGUI_INDX_T5", "Active Players" );
-define( "LANGUI_INDX_T6", "Players Online" );
-define( "LANGUI_INDX_T7", "  <h1>TravianX  <font color=\"#9bd7f9\">Server </font></h1>\r\n  <h5>TravianX is one of the most successful browsers games in the world. TravianX in based on the art of war, expand and train an army to protect them, in the end, to assist one another with the other players competed for the construction of a miracle world and contains game</h5>" );
-define( "LANGUI_INDX_T8", "know the rules of the game" );
-define( "LANGUI_INDX_T9", "register now" );
-define( "LANGUI_INDX_T10", "<li>\r\n      <h3>TravianX</h3>\r\n\t  <h4>Travian Server</h4>\r\n      <span>assets/indx/img/1.jpg</span>\r\n      <p>TravianX is one of the most successful browsers games in the world. TravianX in based on the art of war, establish, expand and train an army to protect your villages</p>\r\n      <a href=\"#\"><img src=\"assets/indx/img/3thumb.gif\" alt=\"Orange Fish\" /></a>\r\n\t </li>\r\n    <li>\r\n      <h3>Miracle</h3>\r\n      <span>assets/indx/img/2.jpg</span>\r\n      <p>Assist one another with the other players competed for the construction of a miracle the world and contains the game on the three tribes: Romans, Gauls, a tribe DBOOR.</p>\r\n      <img src=\"assets/indx/img/2thumb.gif\" alt=\"Sea Turtle\" />\r\n\t</li>\r\n    <li>\r\n      <h3>Slingshot</h3>\r\n      <span>assets/indx/img/3.jpg</span>\r\n      <p>this is the best and powerful unit in game, where it can be used to destruct the building but it is slightly has a slow speed .</p>\r\n      <img src=\"assets/indx/img/1thumb.gif\" alt=\"Sea Turtle\" />\r\n\t</li>" );
-define( "LANGUI_INDX_T11", "arabic" );
-define( "LANGUI_INDX_T12", "english" );
-define( "LANGUI_INDX_T13", "forum" );
-define( "LANGUI_INDX_T14", "social network" );
+define( "LANGUI_INDX_T4", "Players in total" );
+define( "LANGUI_INDX_T5", "Active players" );
+define( "LANGUI_INDX_T6", "Players online" );
+define( "LANGUI_INDX_T7", "  <h1>TravianX</h1>\r\n<h5>TravianX is one of the most successful browser-based games in the world. As a player in the world of TravianX, you build your own empire, recruit a mighty army and fight with your allies for game world hegemony.</h5>" );
+define( "LANGUI_INDX_T8", "Read the Manual" );
+define( "LANGUI_INDX_T9", "Register" );
+define( "LANGUI_INDX_T10", "<li>\r\n<h3>TravianX</h3>\r\n\t<h4>Travian Server</h4>\r\n<span>assets/indx/img/1.jpg</span>\r\n<p>TravianX is one of the most successful browser-based games in the world. Recruit an army, protect your villages and fight with other players in the epic struggle to rule the world.</p>\r\n<a href=\"#\"><img src=\"assets/indx/img/3thumb.gif\" alt=\"Orange Fish\" /></a>\r\n\t</li>\r\n<li>\r\n<h3>The World Wonder</h3>\r\n      <span>assets/indx/img/2.jpg</span>\r\n<p>Compete with your allies to build the mighty World Wonder.</p>\r\n<img src=\"assets/indx/img/2thumb.gif\" alt=\"Sea Turtle\" />\r\n\t</li>\r\n<li>\r\n<h3>Catapult</h3>\r\n<span>assets/indx/img/3.jpg</span>\r\n<p>Catapult is one of the most powerful units in the game. Despite it's slow attack speed, it can easily destroy building and other constructions.</p>\r\n<img src=\"assets/indx/img/1thumb.gif\" alt=\"Sea Turtle\" />\r\n\t</li>" );
+define( "LANGUI_INDX_T11", "Arabic" );
+define( "LANGUI_INDX_T12", "English" );
+define( "LANGUI_INDX_T13", "Forum" );
+define( "LANGUI_INDX_T14", "Social Network" );
 ?>
