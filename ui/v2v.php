191b84d61c02b617ba52c2018323015fb643151c
FeraL <feral@zravian.com>
2012-05-25 15:13:20 +0200

    More language fixes.

--- ui/v2v.php
+++ ui/v2v.php
@@ -8,20 +8,20 @@
 /*********************/
 
 define( "LANGUI_V2V_T1", "Send troops" );
-define( "LANGUI_V2V_T2", "support" );
-define( "LANGUI_V2V_T3", "village" );
-define( "LANGUI_V2V_T4", "Attack: attack" );
-define( "LANGUI_V2V_T5", "Attack: raid" );
-define( "LANGUI_V2V_T6", "target" );
-define( "LANGUI_V2V_T7", "player" );
-define( "LANGUI_V2V_T8", "units" );
-define( "LANGUI_V2V_T9", "resources" );
+define( "LANGUI_V2V_T2", "Reinforcement" );
+define( "LANGUI_V2V_T3", "Village" );
+define( "LANGUI_V2V_T4", "Normal attack" );
+define( "LANGUI_V2V_T5", "Raid" );
+define( "LANGUI_V2V_T6", "Target" );
+define( "LANGUI_V2V_T7", "Player" );
+define( "LANGUI_V2V_T8", "Units" );
+define( "LANGUI_V2V_T9", "Resources" );
 define( "LANGUI_V2V_T10", "Options" );
-define( "LANGUI_V2V_T11", "spy the resources and troops" );
-define( "LANGUI_V2V_T12", "spy the fortifications and troops" );
-define( "LANGUI_V2V_T13", "random" );
-define( "LANGUI_V2V_T14", "will be attacking" );
-define( "LANGUI_V2V_T15", "second goal" );
-define( "LANGUI_V2V_T16", "access" );
+define( "LANGUI_V2V_T11", "Scout resources and troops" );
+define( "LANGUI_V2V_T12", "Scout defences and troops" );
+define( "LANGUI_V2V_T13", "Random" );
+define( "LANGUI_V2V_T14", "Will be attacking" );
+define( "LANGUI_V2V_T15", "Second goal" );
+define( "LANGUI_V2V_T16", "Access" );
 define( "LANGUI_V2V_T17", "hours" );
 ?>
